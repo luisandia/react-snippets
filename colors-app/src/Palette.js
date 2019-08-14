0c6c3538a8d782c2e7b488ee0fdc8805fda7a8ee
luisandia <luisandiamamani@gmail.com>
2019-08-13 20:13:09 -0500

    added copy to clipboard

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import ColorBox from './ColorBox';
+import './Palette.css';
+
 
 export class Palette extends Component {
+
     render() {
+        const colorBoxes = this.props.colors.map(color => <ColorBox key={color.color} background={color.color} name={color.name} />)
         return (
             <div className="Palette">
                 <div className="Palette-colors">
                     {/* navbar does here */}
+                    {colorBoxes}
                 </div>
                 {/* footer */}
             </div>
