7ac973611765c39c2bb5d3e60db4b9a70a8e18fd
luisandia <luisandiamamani@gmail.com>
2019-08-13 19:06:35 -0500

    created react app, added palette

---
+++ colors-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+function App() {
+  return (
+    <div className="App">
+      <Palette {...seedColors[4]}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ colors-app/src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                <div className="Palette-colors">
+                    {/* navbar does here */}
+                </div>
+                {/* footer */}
+            </div>
+        )
+    }
+}
+
+export default Palette
