e462ac02e414e7a23586a7548386a15e6444cdb7
luisandia <luisandiamamani@gmail.com>
2019-08-14 15:41:43 -0500

    added color slider

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import {generatePalette} from './colorHelpers';
+
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[4]}/>
+      <Palette palette={generatePalette(seedColors[4])}/>
     </div>
   );
 }
