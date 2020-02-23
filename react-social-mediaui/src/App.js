63dee4d9d230d13c1b6a4188013fecfa93cd0e2d
luisandia <luisandiamamani@gmail.com>
2020-02-22 23:08:23 -0500

    finished backend, starting frontend with menu

--- react-social-mediaui/src/App.js
+++ react-social-mediaui/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+// pages
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ react-social-mediaui/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  color:#eee;
+  text-decoration:none;
+
+`;
+export const StyledAppBar = styled(AppBar)`
+    align-items:center;
+`;
+export class Navbar extends Component {
+  render() {
+    return (
+      <StyledAppBar>
+        <Toolbar >
+          <StyledLink to={'/login'} style={{ color: "white", textDecoration: "none" }}>
+            <Button color="inherit" >Login</Button>
+          </StyledLink>
+          <StyledLink to={'/'}>
+            <Button color="inherit">Home</Button>
+          </StyledLink>
+          <StyledLink to={'/signup'}>
+            <Button color="inherit">SignUp</Button>
+          </StyledLink>
+        </Toolbar>
+      </StyledAppBar>
+
+    )
+  }
+}
+
+export default Navbar
