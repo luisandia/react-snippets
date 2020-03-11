55ca30ab15c9992368b649d90d34bff80d21d613
luisandia <luisandiamamani@gmail.com>
2020-03-10 23:15:17 -0500

    working on cartpage

--- react-tech-store/src/pages/CartPage.js
+++ react-tech-store/src/pages/CartPage.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
+import cartBcg from "../images/storeBcg.jpeg";
+import CartSection from "../components/CartPage";
 
-export default function CartPage() {
+export default function CartPage(props) {
   return (
     <>
-      <h1>Hello cart page</h1>
+      <Hero img={cartBcg} />
+      <CartSection history={props.history} />
     </>
-  )
+  );
 }
