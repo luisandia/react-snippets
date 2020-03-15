71e503189fda20df1cae05baea5a748043edcbd1
luisandia <luisandiamamani@gmail.com>
2020-03-14 23:27:21 -0500

    asign user to order

--- react-store-strapi/frontend/src/pages/Products.js
+++ react-store-strapi/frontend/src/pages/Products.js
@@ -3,7 +3,7 @@ import { ProductContext } from "../context/products";
 import Loading from "../components/Loading";
 import ProductList from "../components/Products/ProductList";
 import PageProducts from "../components/Products/PageProducts";
-// import Filters from "../components/Products/Filters";
+import Filters from "../components/Products/Filters";
 export default function Products() {
   const { loading, products } = React.useContext(ProductContext);
 
@@ -12,7 +12,7 @@ export default function Products() {
   }
   return (
     <>
-      {/* <Filters /> */}
+      <Filters />
       <PageProducts></PageProducts>
     </>
   );
--- react-store-strapi/strapi/api/orders/controllers/orders.js
+++ react-store-strapi/strapi/api/orders/controllers/orders.js
@@ -1,8 +1,23 @@
 'use strict';
-
+const stripe = require('stripe')('sk_test_ImYaOvfhGIkFcTo9eBxQvv2i')
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create: async ctx => {
+    const { name, total, items, stripeTokenId } = ctx.request.body;
+    const { id } = ctx.state.user;
+    const charge = await stripe.charges.create({
+      amount: Math.round(total * 100),
+      currency: "usd",
+      source: stripeTokenId,
+      description: `Order ${new Date()} By ${ctx.state.user.username}`
+    });
+    const order = await strapi.services.orders.create({
+      name, total, items, user: id
+    });
+    return order;
+  }
+};
